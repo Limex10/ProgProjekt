f91f4595e91cbdcd9cc35747f104d70d0e32c01c
Limex <Limex@DESKTOP-1R16QSV>
2019-01-05 00:45:21 +0100

    fixed picking up keys and going thgrough doors

--- ProgProjekt/Source.c
+++ ProgProjekt/Source.c
@@ -6,67 +6,166 @@ int main(void)
 	int width = 20;
 	int height = 20;
 	int walls = 10;
+	char door = ' ';
+	char key = ' ';
 	MapT tMap;
 
 	positionT *tPosition = (positionT*)malloc(sizeof(positionT));
 
+	int nrKeys = 0;
 	int x = 0;
 	int y = 0;
 	char selector;
 	tMap = createMap(width, height, walls);
 	placeObject(tMap, x, y, '@', tPosition, 1);
+	
 
 
 	while (1)
 	{
+		
 		system("cls");
+		printf("%d", nrKeys);
 		drawMap(tMap);
+		
 		selector = getchar();
 		switch (selector)
 		{
 
 		case 'w':
 		{
-			placeObject(tMap, tPosition->row - 1, tPosition->col, '@', tPosition, 1);
-			removeObject(tMap, tPosition->row + 1, tPosition->col, ' ', tPosition, 1);
-			if (keyChecker(tMap, tPosition->row - 1, tPosition->col, '@', tPosition, 1) == 1)
-			{
-				
-				printf("du har hittat en nyckel");
 
-				getchar();
+			door = placeObject(tMap, tPosition->row - 1, tPosition->col, '@', tPosition, 1);
+			key = door;
+			if (key == 'K')
+			{
+
+				printf("du har hittat en nyckel");
+				nrKeys++;
 				getchar();
 			}
-			
+			else if (door == 'D')
+			{
+				if (nrKeys > 0)
+				{
+					doorMover(tMap, tPosition->row - 1, tPosition->col, '@', tPosition, 1);
+
+					printf("du har låst upp en dörr");
+					nrKeys--;
+					getchar();
+				}
+				else
+				{
+					printf("du har inte nog med nycklar");
+					getchar();
+					getchar();
+				}
+			}
 			removeObject(tMap, tPosition->row + 1, tPosition->col, ' ', tPosition, 1);
-			
-			
 			
 			break;
 		}
 
 		case 'a':
-		{
-			
-			placeObject(tMap, tPosition->row , tPosition->col - 1, '@', tPosition, 1);
+		{	
+
+			door = placeObject(tMap, tPosition->row, tPosition->col - 1, '@', tPosition, 1);
+			key = door;
+			if (key == 'K')
+			{
+
+				printf("du har hittat en nyckel");
+				nrKeys++;
+				getchar();
+			}
+			else if (door == 'D')
+			{
+				if (nrKeys > 0)
+				{
+					doorMover(tMap, tPosition->row, tPosition->col - 1, '@', tPosition, 1);
+
+					printf("du har låst upp en dörr");
+					nrKeys--;
+					getchar();
+				}
+				else
+				{
+					printf("du har inte nog med nycklar");
+					getchar();
+					getchar();
+				}
+			}
 			removeObject(tMap, tPosition->row, tPosition->col + 1, ' ', tPosition, 1);
-			keyChecker(tMap, tPosition->row, tPosition->col - 1, '@', tPosition, 1);
 
 			break;
 		}
 
 		case 'd':
 		{
-			placeObject(tMap, tPosition->row, tPosition->col + 1, '@', tPosition, 1);
+	
+				door = placeObject(tMap, tPosition->row, tPosition->col + 1, '@', tPosition, 1);
+				key = door;
+			if (key == 'K')
+			{
+
+				printf("du har hittat en nyckel");
+				nrKeys++;
+				getchar();
+			}
+			else if (door == 'D')
+			{
+				if(nrKeys > 0)
+				{
+					doorMover(tMap, tPosition->row, tPosition->col + 1, '@', tPosition, 1);
+
+					printf("du har låst upp en dörr");
+					nrKeys--;
+					getchar();
+				}
+				else
+				{
+					printf("du har inte nog med nycklar");
+					getchar();
+					getchar();
+				}
+				
+			}
 			removeObject(tMap, tPosition->row, tPosition->col - 1, ' ', tPosition, 1);
-			keyChecker(tMap, tPosition->row, tPosition->col + 1, '@', tPosition, 1);
 			break;
 		}
+
 		case 's':
 		{
-			placeObject(tMap, tPosition->row + 1, tPosition->col, '@', tPosition, 1);
+
+			door = placeObject(tMap, tPosition->row + 1, tPosition->col , '@', tPosition, 1);
+			key = door;
+			if (key == 'K')
+			{
+
+				printf("du har hittat en nyckel");
+				nrKeys++;
+				getchar();
+			}
+			else if (door == 'D')
+			{
+				if (nrKeys > 0)
+				{
+					doorMover(tMap, tPosition->row + 1, tPosition->col, '@', tPosition, 1);
+
+					printf("du har låst upp en dörr");
+					nrKeys--;
+					getchar();
+				}
+				else
+				{
+					printf("du har inte nog med nycklar");
+					getchar();
+					getchar();
+				}
+			}
+
 			removeObject(tMap, tPosition->row - 1, tPosition->col, ' ', tPosition, 1);
-			keyChecker(tMap, tPosition->row + 1, tPosition->col, '@', tPosition, 1);
+
 			break;
 		}
 
@@ -77,7 +176,6 @@ int main(void)
 
 		
 	}
-
 
 	return 0;
 }
--- ProgProjekt/houseHelper.c
+++ ProgProjekt/houseHelper.c
@@ -22,6 +22,19 @@ int  placeObject(MapT tMap, int row, int col, char chObj, positionT *pos, int vi
 			col = rand() % (tMap.width - 2) + 1;
 		} 
 	}
+	if (tMap.mArr[row][col] == 'K')
+	{
+		tMap.mArr[row][col] = chObj;
+		tMap.vArr[row][col] = visibility;
+		pos->row = row;
+		pos->col = col;
+		return 'K';
+	}
+	if (tMap.mArr[row][col] == 'D')
+	{
+		
+		return 'D';
+	}
 	if (tMap.mArr[row][col] != ' ')
 		return 0;
 	tMap.mArr[row][col] = chObj;
@@ -50,8 +63,9 @@ int  removeObject(MapT tMap, int row, int col, char chObj, positionT *pos, int v
 	return 0;
 }
 
-int  keyChecker(MapT tMap, int row, int col, char chObj, positionT *pos, int visibility)
+int  doorMover(MapT tMap, int row, int col, char chObj, positionT *pos, int visibility)
 {
+
 	if (row < 0 || row > tMap.heigth - 1 || col < 0 || col > tMap.width - 1) //Outside housemap
 		return 0;
 	if (row == 0 && col == 0) { //Select random values for row and col
@@ -60,18 +74,12 @@ int  keyChecker(MapT tMap, int row, int col, char chObj, positionT *pos, int vis
 			col = rand() % (tMap.width - 2) + 1;
 		}
 	}
-	if (tMap.mArr[row][col] != 'K')
-		return 0;
-	else
-		/*
-			tMap.mArr[row][col] = chObj;
+
+	tMap.mArr[row][col] = chObj;
 	tMap.vArr[row][col] = visibility;
 	pos->row = row;
 	pos->col = col;
-		*/
-
-		printf("en nyckel");
-	return 1;
+	return 0;
 }
 
 
